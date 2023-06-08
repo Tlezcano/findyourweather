caa28caa8b8232d699ec7cfb1dc38112b209aa57
Juan9Gomez <aguilerajuan649@gmail.com>
2023-06-08 10:53:49 -0300

    modified:   users.js

--- users.js
+++ users.js
@@ -1,5 +1,6 @@
 let users = new Map();
 users.set("thiago", {user: "thiago", password: "123"});
 users.set("daniel", {user: "daniel", password: "1234"});
+users.set("golea", {user: "golea", password: "12345"});
  
 export default users;
